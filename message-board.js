3d436cafc98d992a9a7cc9757768a76c442ff89c
Cajulis Manry, Amber <amber.manry@icfi.com>
2016-01-16 23:42:05 -0500

    allow user to enter new message

--- message-board.js
+++ message-board.js
@@ -7,4 +7,23 @@ if (Meteor.isClient) {
             return Messages.find({}, {sort: {createdAt: 1}});
         }
     });
+
+    Template.body.events({
+        "submit .new-message": function (event) {
+            // Prevent default browser form submit
+            event.preventDefault();
+
+            // Get value from form element
+            var text = event.target.text.value;
+
+            // Insert a task into the collection
+            Messages.insert({
+                text: text,
+                createdAt: new Date() // current time
+            });
+
+            // Clear form
+            event.target.text.value = "";
+        }
+    });
 }
