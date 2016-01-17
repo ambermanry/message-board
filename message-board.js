7aa5990f0d46f0be08fd8663fd75bc527b811ae9
Cajulis Manry, Amber <amber.manry@icfi.com>
2016-01-16 23:04:09 -0500

    incorporate mongo

--- message-board.js
+++ message-board.js
@@ -1,10 +1,10 @@
+Messages = new Mongo.Collection("messages");
+
 if (Meteor.isClient) {
     // This code only runs on the client
     Template.body.helpers({
-        messages: [
-            { text: "My First Post" },
-            { text: "What do you think?" },
-            { text: "The quick brown fox jumped over the lazy dog." }
-        ]
+        messages: function () {
+            return Messages.find({}, {sort: {createdAt: 1}});
+        }
     });
 }
